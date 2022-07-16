78ff9b39c90f66cf3a2320b3f3eeff2ac8d75d5f
Jelhill <talk2jelili2000@gmail.com>
2022-07-16 20:38:30 +0100

    Another push to test

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json("Welcome: This app is running well on port 5000")
+    res.json("Welcome: This app is running well")
 })
 app.listen(5000, console.log("Server running on port 5000"))
