ca82dfc49b3005d6062b2259e3ef867c7fff215a
Jelhill <talk2jelili2000@gmail.com>
2022-07-17 18:57:08 +0100

    Update

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json("Welcome: This app is running well")
+    res.json("Welcome: This is a test app and it is running well")
 })
 app.listen(5000, console.log("Server running on port 5000"))
