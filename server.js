8c18af7eff58075c5962c3c7d81bf281c350f98b
Jelhill <talk2jelili2000@gmail.com>
2022-07-19 12:27:03 +0100

    update

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).json({ success: true, message: "App is running successfully"})
+    res.status(200).json({ success: true, message: "App is running successfully and deploying"})
 })
 app.listen(5000, console.log("Server running on port 5000"))
